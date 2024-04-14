34f7ea68f9fc52846301db84abed148ee28c0f96
Truong Le <truongle@Truongs-MacBook-Pro.local>
2024-04-14 16:12:30 +0700

    apply filter meals

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -9,12 +9,14 @@ class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({
     super.key,
     required this.onToggleFavorite,
+    required this.availableMeals,
   });
 
   final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filterMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    final filterMeals = availableMeals.where((meal) => meal.categories.contains(category.id)).toList();
 
     Navigator.of(context).push(
       MaterialPageRoute(
